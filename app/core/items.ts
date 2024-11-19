ee8fc48448b202ea7e58c5b252fafaf218c05524
Exeatop <164261742+Exeatop@users.noreply.github.com>
2024-11-19 03:10:24 -0500

    Added ./app/core/items.ts

---
+++ app/core/items.ts
@@ -0,0 +1,95 @@
+import { EffectClass } from "../types/enum/EffectClass"
+import Board from "./board"
+import { PokemonEntity } from "./pokemon-entity"
+import { Item } from "../types/enum/Item"
+import PokemonState from "./pokemon-state"
+import { AbilityStrategies } from "./abilities/abilities"
+import { Ability } from "../types/enum/Ability"
+
+export function triggerItemEffects(
+    effect_class: EffectClass,
+    params: {
+        pokemon: PokemonEntity,
+        state: PokemonState,
+        board: Board,
+        target: PokemonEntity,
+        crit: boolean
+}){
+    params.pokemon.items.forEach((i) =>{
+        const handler = itemMap.get(i)
+        if(handler){
+            handler(effect_class, params)
+        } else{
+            console.error(`${i} missing from itemMap`)
+        }
+    })
+}
+
+function triggerAquaEgg(
+    effect_class: EffectClass,
+    params: {
+        pokemon: PokemonEntity,
+        board: Board
+}){
+    switch (effect_class){
+        case EffectClass.ABILITY:
+            params.pokemon.addPP(20, params.pokemon, 0, false)
+            break
+    }
+}
+
+function triggerStarDust(
+    effect_class: EffectClass,
+    params: {
+        pokemon: PokemonEntity,
+        board: Board
+}){
+    switch(effect_class){
+        case EffectClass.ABILITY:
+            params.pokemon.addShield(Math.round(0.5 * params.pokemon.maxPP), params.pokemon, 0, false)
+            params.pokemon.count.starDustCount++
+            break
+    }
+}
+
+function triggerLeppaBerry(
+    effect_class: EffectClass,
+    params: {pokemon: PokemonEntity
+}){
+    switch(effect_class){
+        case EffectClass.ABILITY:
+            params.pokemon.eatBerry(Item.LEPPA_BERRY)
+            break
+    }
+}
+
+function triggerComfey(
+    effect_class: EffectClass,
+    params: {
+        pokemon: PokemonEntity,
+        state: PokemonState,
+        board: Board,
+        target: PokemonEntity,
+        crit: boolean
+}){
+    switch(effect_class){
+        case EffectClass.ABILITY:
+            AbilityStrategies[Ability.FLORAL_HEALING].process(
+            params.pokemon,
+            params.state,
+            params.board,
+            params.target,
+            params.crit,
+            true
+            )
+            break
+    }
+}
+
+
+const itemMap: ReadonlyMap<Item, (...args)=>void> = new Map([
+    [Item.AQUA_EGG, triggerAquaEgg],
+    [Item.STAR_DUST, triggerStarDust],
+    [Item.LEPPA_BERRY, triggerLeppaBerry],
+    [Item.COMFEY, triggerComfey]
+])
